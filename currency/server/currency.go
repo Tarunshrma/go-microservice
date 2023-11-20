d4eaab0b941889ab9e34b178ff92166d8415cf9f
tarunsharma <tarun.kr.sharma@hotmail.com>
2023-11-20 16:56:49 +0400

    Updated grpc client to add bi directional streaming to pub-sub rate exchange data

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -10,12 +10,41 @@ import (
 )
 
 type Currency struct {
-	log   hclog.Logger
-	rates *data.ExchangeRates
+	log           hclog.Logger
+	rates         *data.ExchangeRates
+	subscriptions map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest
 }
 
 func NewCurrency(l hclog.Logger, r *data.ExchangeRates) *Currency {
-	return &Currency{l, r}
+	c := &Currency{l, r, make(map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest)}
+	go c.handleUpdates()
+
+	return c
+}
+
+func (c *Currency) handleUpdates() {
+	ru := c.rates.MonitorRates(5 * time.Second)
+	for range ru {
+		c.log.Info("Got Updated rates")
+
+		// loop  subscribed clients
+		for k, v := range c.subscriptions {
+
+			// loop  subscribed rates
+			for _, rr := range v {
+				r, err := c.rates.GetRate(rr.GetBase().String(), rr.GetDestination().String())
+				if err != nil {
+					c.log.Error("Unable to get update rate", "base", rr.GetBase().String(), "destination", rr.GetDestination().String())
+				}
+
+				err = k.Send(&protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: r})
+				if err != nil {
+					c.log.Error("Unable to send updated rate", "base", rr.GetBase().String(), "destination", rr.GetDestination().String())
+				}
+			}
+		}
+
+	}
 }
 
 func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
@@ -27,40 +56,37 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 		return nil, err
 	}
 
-	return &protos.RateResponse{Rate: r}, nil
+	return &protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: r}, nil
 }
 
 func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
 
 	// handle client messages
-	go func() {
-		for {
-			rr, err := src.Recv()
-			if err == io.EOF {
-				c.log.Info("Client has closed connection")
-				break
-			}
-
-			// any other error means the transport between the server and client is unavailable
-			if err != nil {
-				c.log.Error("Unable to read from client", "error", err)
-				break
-			}
-
-			c.log.Info("Handle client request", "request_base", rr.GetBase(), "request_dest", rr.GetDestination())
-		}
-	}()
-
-	// handle server responses
-	// we block here to keep the connection open
 	for {
-		err := src.Send(&protos.RateResponse{Rate: 19.2})
-		if err != nil {
-			return err
+		rr, err := src.Recv()
+		if err == io.EOF {
+			c.log.Info("Client has closed connection")
+			break
 		}
 
-		time.Sleep(5 * time.Second)
+		// any other error means the transport between the server and client is unavailable
+		if err != nil {
+			c.log.Error("Unable to read from client", "error", err)
+			break
+		}
+
+		c.log.Info("Handle client request", "request_base", rr.GetBase(), "request_dest", rr.GetDestination())
+
+		rrs, ok := c.subscriptions[src]
+		if !ok {
+			rrs = []*protos.RateRequest{}
+		}
+
+		rrs = append(rrs, rr)
+		c.subscriptions[src] = rrs
 	}
+
+	return nil
 }
 
 //func (c *Currency) MustEmbedUnimplementedCurrencyServer() {
